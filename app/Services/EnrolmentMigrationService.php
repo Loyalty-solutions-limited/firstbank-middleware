dfaac0a5499ad6e0576619f116c52f29de960a25
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2025-06-03 10:42:26 +0100

    update

--- app/Services/EnrolmentMigrationService.php
+++ app/Services/EnrolmentMigrationService.php
@@ -60,7 +60,7 @@ public static function migrateEnrolments1() : string
 
         //dd($company_details);
 
-        $pendingEnrolments = Enrollment::where('enrollment_status',0)->where('tries', '<=', 4)->limit(500);//->get();//->where('tries', '<', 5);//->get();
+        $pendingEnrolments = Enrollment::where('enrollment_status',0)->where('tries', '<=', 4)->limit(5);//->get();//->where('tries', '<', 5);//->get();
 
         // return response()->json(['data' => $pendingEnrolments]);
         // $pendingEnrolments = Enrollment::where('enrollment_status',0)->where('tries', '<=', 4)->select('first_name' ,'last_name', 'email','enrollment_status', 'tries', 'cif_id', 'branch_code', 'accountnumber', 'cif_id', 'pin', 'password')->limit(1000);//->get();//->where('tries', '<', 5);//->get();
