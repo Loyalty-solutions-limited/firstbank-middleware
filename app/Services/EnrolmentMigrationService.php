f9d7d696feeaec0fa27ddd9bb3037f98d58c6441
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2025-06-04 16:01:40 +0100

    debugging

--- app/Services/EnrolmentMigrationService.php
+++ app/Services/EnrolmentMigrationService.php
@@ -129,7 +129,7 @@ public static function migrateEnrolments1()
                     } catch (\Exception $ex) {
                         throw new \Exception("Something went wrong " . $ex->getMessage());
                     }
-                    dd($resp);
+                    print_r($resp); return;
                     if (parent::isJSON($resp))
                     {
                         $repsonse = json_decode($resp, true);
