308a3f6ce228192cd555a8ce17ebba5164b3f696
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2025-06-04 12:45:27 +0100

    remove send-main-api

--- routes/web.php
+++ routes/web.php
@@ -76,7 +76,7 @@ Route::post('/get_biller_items', [BAPController::class, 'getBillerItems']);
 Route::post('/sendbill_payment_advice', [BAPController::class, 'sendBillPaymentAdvice']);
 Route::post('/log-emails', [LogEmailsController::class, 'log']);
 Route::post('/send-email-API', [LogEmailsController::class, 'sendMail']);
-Route::post('/send-email-SMTP', [LogEmailsController::class, 'sendWithSMTP']);
+// Route::post('/send-email-SMTP', [LogEmailsController::class, 'sendWithSMTP']);
 Route::get('/get-mail-parameters', [LogEmailsController::class, 'getMailParameters']);
 Route::get('/test-route', [EnrollmentController::class, 'test_db']);
 Route::get('/trans-test', [EnrollmentController::class, 'test_trans']);
