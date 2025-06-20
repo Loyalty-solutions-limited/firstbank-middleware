d038c0bc94aea49b54a62c270b23eb1c169f25d4
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2025-06-20 17:41:26 +0100

    debugging

--- app/Http/Controllers/LogEmailsController.php
+++ app/Http/Controllers/LogEmailsController.php
@@ -110,7 +110,7 @@ class LogEmailsController extends Controller
         'Cookie' => 'ARRAffinity=8cb9eb8a9c8e49bb32964ef5e087477636164e3b1bd119e62b62b2d516d04b33; ARRAffinitySameSite=8cb9eb8a9c8e49bb32964ef5e087477636164e3b1bd119e62b62b2d516d04b33'
         ];
 
-        // print_r($headers);
+        print_r(json_encode($request->all()));
 
         $request = new GuzzleRequest('POST', $url, $headers, json_encode($request->all()));
         $res = $client->sendAsync($request)->wait();
