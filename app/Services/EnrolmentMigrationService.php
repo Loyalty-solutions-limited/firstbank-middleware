759ecae09ec90488f85de7b018fa7809f663e3d4
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2025-06-24 07:57:34 +0100

    dumping data

--- app/Services/EnrolmentMigrationService.php
+++ app/Services/EnrolmentMigrationService.php
@@ -76,7 +76,7 @@ public static function migrateEnrolments1()
 
         // return response()->json(['data' => $pendingEnrolments]);
         // $pendingEnrolments = Enrollment::where('enrollment_status',0)->where('tries', '<=', 4)->select('first_name' ,'last_name', 'email','enrollment_status', 'tries', 'cif_id', 'branch_code', 'accountnumber', 'cif_id', 'pin', 'password')->limit(1000);//->get();//->where('tries', '<', 5);//->get();
-        // return $pendingEnrolments->count();
+        return $pendingEnrolments;
        if ($pendingEnrolments->count()>0)
        {
             foreach($pendingEnrolments->unique('cif_id') as $pendingEnrolment)
