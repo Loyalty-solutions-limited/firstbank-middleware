b490ce71b1ef2fed5051a90b115b8200311c4a4e
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2025-06-27 16:50:02 +0100

    debug

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -13,7 +13,7 @@ class SendMailController extends Controller
     public function sendMailNew(Request $request)
     {
         $req = $request->all();
-        return $request->body . " " . $request->acid . $request['body'] . $request['acid'];
+        return $request->body . " " . $request->acid . $request['body'] . $request['acid'] . $request['requestId'] . $request->requestId;
         $data = [
             'body' => "Hello world!",
             'acid' => "MO53791387",
