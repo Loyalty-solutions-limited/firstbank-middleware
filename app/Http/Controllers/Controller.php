14649ad44c5d2f122169e5846c96cee0cbab9df5
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2025-06-27 16:09:25 +0100

    change sendmail

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use GuzzleHttp\Psr7\Request as GuzzleRequest;
+use GuzzleHttp\Client;
 
 class Controller extends BaseController
 {
@@ -56,5 +58,30 @@ class Controller extends BaseController
 
         return response()->json($response,404);
     }
+
+    public static function sendMailGuzzle($request)
+{
+        $url = env('POINTS_TO_CASH_URL') . "email/send-email";
+
+
+        // $res = Http::withOptions(['verify' => false])->post($url);
+
+        $client = new Client(['verify' => false]);
+        $headers = [
+        'Accept' => 'application/json',
+        'AppId' => config("externalservices.POINTS_TO_CASH_APPID"),
+        'AppKey' => config("externalservices.POINTS_TO_CASH_APPKEY"),
+        'Content-Type' => 'application/json',
+        'Cookie' => 'ARRAffinity=8cb9eb8a9c8e49bb32964ef5e087477636164e3b1bd119e62b62b2d516d04b33; ARRAffinitySameSite=8cb9eb8a9c8e49bb32964ef5e087477636164e3b1bd119e62b62b2d516d04b33'
+        ];
+
+        // print_r(json_encode($request->all()));
+
+        $request = new GuzzleRequest('POST', $url, $headers, json_encode($request));
+        $res = $client->sendAsync($request)->wait();
+        echo $res->getBody();
+
+
+}
 
 }
