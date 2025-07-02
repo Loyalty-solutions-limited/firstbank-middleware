87a047f78528d6052c7516c4936ee2877fa8ed54
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2025-07-02 21:28:55 +0100

    add makeCurl

--- app/Http/Controllers/PTCController.php
+++ app/Http/Controllers/PTCController.php
@@ -44,4 +44,35 @@ class PTCController extends Controller
 
         return json_decode($this->makeCurl($url, 'POST', $data));
     }
+
+    public function makeCurl($url, $action, $data)
+    {
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => $action,
+    CURLOPT_POSTFIELDS =>$data,
+    CURLOPT_HTTPHEADER => array(
+        'Accept: application/json',
+        "AppId: ".config("externalservices.POINTS_TO_CASH_APPID"),
+        "AppKey: ".config("externalservices.POINTS_TO_CASH_APPKEY"),
+        'Content-Type: application/json',
+        'Cookie: ARRAffinity=8cb9eb8a9c8e49bb32964ef5e087477636164e3b1bd119e62b62b2d516d04b33; ARRAffinitySameSite=8cb9eb8a9c8e49bb32964ef5e087477636164e3b1bd119e62b62b2d516d04b33'
+    ),
+    ));
+
+    $response = curl_exec($curl);
+
+
+    curl_close($curl);
+
+    return $response;
+    }
 }
