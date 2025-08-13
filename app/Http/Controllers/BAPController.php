44a25f59cb86f5157eb0d8b414f828e6367305b8
kenwizzy <nwidehifeanyi@yahoo.com>
2025-08-13 09:50:37 +0100

    Returned billsPaymentAdvice response from BAP

--- app/Http/Controllers/BAPController.php
+++ app/Http/Controllers/BAPController.php
@@ -56,9 +56,9 @@ class BAPController extends BaseController
                         <CustomerEmail>a@b.com</CustomerEmail>
                         <Narration>Trans_ref$trans_ref/$request->customer_account_number/$request->customer_mobile</Narration>
                     </BillPaymentAdvice>";
-            return $payload;
+            //$payload;
             
-            return $this->postDataGuzzle($payload, "SendBillPaymentAdvice");
+            dd($this->postDataGuzzle($payload, "SendBillPaymentAdvice"));
              //return $this->postData($payload, "SendBillPaymentAdvice");
 
     }
